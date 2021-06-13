eee15ae94b0ec767d6786b0871633a205108c5f5
qzhou19 <zhouqianrr@gmail.com>
2021-06-13 16:16:23 -0500

    add make steps lint with intentional error in hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,16 @@
 import click
+var=
 
 @click.command(help="This is just a hello app. It does nothing.")
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
     if name == "Thor":
-        click.echo("Thor, you are always red.")
-        click.echo(click.style(f"Hello {name}!", fg="red"))
+        click.secho("Thor, you are always red.")
+        click.secho(click.style(f"Hello {name}!", fg="red"))
     else:
-        click.echo(f"Your color is {color}!")
-        click.echo(click.style(f"Hello {name}!", fg=color))
+        click.secho(f"Your color is {color}!")
+        click.secho(click.style(f"Hello {name}!", fg=color))
 
 if __name__ == "__main__":
     hello()
